c358f2e7d93e39f8eb27edabbf69b18b941b7029
kimkwatson <kimwatson731@gmail.com>
2026-01-26 22:26:44 -0700

    added controller functions and routes for POST PUT DELETE

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,6 +1,7 @@
 const mongodb = require("../db/connect");
 const { ObjectId } = require("mongodb");
 
+// get all contacts
 const getAllContacts = async (req, res) => {
     try {
       const db = mongodb.getDb().db();
@@ -13,6 +14,7 @@ const getAllContacts = async (req, res) => {
     }
 };
 
+// get one contact by id
 const getContactById = async (req, res) => {
   try {
     const db = mongodb.getDb().db();
@@ -41,4 +43,56 @@ const getContactById = async (req, res) => {
   }
 };
 
-module.exports = { getAllContacts, getContactById };
+// create new contact
+const createContact = async (req, res) => {
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  try {
+    const db = mongodb.getDb().db();
+    const contactsCollection = db.collection("contacts");
+    const response = await contactsCollection.insertOne(contact);
+    return res.status(201).json({ id: response.insertedId });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// update contact by id
+const updateContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const response = await mongodb
+    .getDb()
+    .db()
+    .collection("contacts")
+    .replaceOne({ _id: userId }, contact);
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(404).json(response.error || 'Some error occurred while updating the contact.');
+  }
+};
+
+// delete contact by id
+const deleteContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const response = await mongodb.getDb().db().collection("contacts").deleteOne({ _id: userId }, true);
+  if (response.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(404).json(response.error || 'Some error occurred while deleting the contact.');
+  }
+}
+
+module.exports = { getAllContacts, getContactById, createContact, updateContact, deleteContact };
--- routes/contacts.js
+++ routes/contacts.js
@@ -7,4 +7,13 @@ router.get("/", contactsController.getAllContacts);
 // GET one contact by id
 router.get("/:id", contactsController.getContactById);
 
+// CREATE new contact
+router.post("/", contactsController.createContact);
+
+// UPDATE contact by id
+router.put("/:id", contactsController.updateContact);
+
+// DELETE contact by id
+router.delete("/:id", contactsController.deleteContact);
+
 module.exports = router;
